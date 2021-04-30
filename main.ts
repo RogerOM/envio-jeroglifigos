62a8a1c906465307262995907e26bd85dd254bfe
Roger O <43441351+RogerOM@users.noreply.github.com>
2021-04-30 11:42:53 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("comida patatas")
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("bebida")
+})
+basic.forever(function () {
+	
+})
